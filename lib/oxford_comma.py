ea8c17bc2d1b30406a3b6e6739aa78c5da4176bb
Janemwaura <mwaura461@gmail.com>
2024-01-21 18:39:13 +0300

    complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,18 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(words):
+    if len(words) == 0:
+        return ""
+
+    if len(words) == 1:
+        return words[0]
+
+    if len(words) == 2:
+        return f"{words[0]} and {words[1]}"
+    
+    result = ", ".join(words[:-1]) + f", and {words[-1]}"
+
+    return result
+
+
+word_list = ["Jane", "oli", "ruth", "george"]
+result_string = oxford_comma(word_list)
+print(result_string)
